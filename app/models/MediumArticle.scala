478a17a9162ba776543c5bd3293ac1213608fb18
Shinichi NOMURA <shinichi.nomura@gmail.com>
2016-06-22 01:47:15 +0900

    記事にメディア名を表示

--- app/models/MediumArticle.scala
+++ app/models/MediumArticle.scala
@@ -1,6 +1,11 @@
 package models
 
 import org.joda.time.DateTime
+import repositories.MediumRepository
 
-case class MediumArticle (url: String, medium_id: Long, title: String, description: String, published_at: DateTime, retrieved_at: DateTime)
+case class MediumArticle (url: String, medium_id: Long, title: String, description: String, published_at: DateTime, retrieved_at: DateTime) {
+  def medium: Medium = {
+    MediumRepository.findById(medium_id).get
+  }
+}
 
